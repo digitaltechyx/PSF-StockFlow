1aff8dea41489c5c9319227858f0951800dad831
digitaltechyx <digitaltechyx@gmail.com>
2025-10-24 03:58:24 +0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,11 @@ import { getFirestore } from "firebase/firestore";
 // IMPORTANT: Replace with your own Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "psf-stockflow.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "psf-stockflow",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "psf-stockflow.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "478462798107",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:478462798107:web:ea5fbbf3927c58373e8bbe",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID  ,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
